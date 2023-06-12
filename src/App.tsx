4413f5990bef4c2fc003bc7e719979e7cdd0be98
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-06-12 22:58:32 +0530

    fix issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,146 +1,68 @@
+import { Suspense, useRef } from "react";
 import * as THREE from "three";
-import { Suspense, useRef, useState } from "react";
-import {
-  Canvas,
-  extend,
-  useFrame,
-  useLoader,
-  useThree,
-} from "@react-three/fiber";
-import { Html, Preload, OrbitControls } from "@react-three/drei";
-// import { Popconfirm } from 'antd'
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, useTexture } from "@react-three/drei";
 
-import {
-  CubeTextureLoader,
-  RGBAFormat,
-  CubeCamera, //highlight-line
-  WebGLCubeRenderTarget, //highlight-line
-  LinearMipmapLinearFilter, //highlight-line
-} from "three";
-
-extend({ OrbitControls });
-
-const CameraControls = () => {
-  // Get a reference to the Three.js Camera, and the canvas html element.
-  // We need these to setup the OrbitControls class.
-  // https://threejs.org/docs/#examples/en/controls/OrbitControls
-
-  const {
-    camera,
-    gl: { domElement },
-  } = useThree();
-
-  // Ref to the controls, so that we can update them on every frame with useFrame
-  const controls: any = useRef();
-  useFrame(() => controls?.current?.update());
-  return (
-    <OrbitControls
-      ref={controls}
-      args={[camera, domElement]}
-      autoRotate={true}
-      enableZoom={false}
-    />
-  );
-};
-
-// Loads the skybox texture and applies it to the scene.
-function SkyBox() {
-  // highlight-start
-  const { scene } = useThree();
-  const loader = new THREE.CubeTextureLoader();
-  // The CubeTextureLoader load method takes an array of urls representing all 6 sides of the cube.
-  const texture = loader.load([
-    "space/space1.jpg",
-    "space/space2.jpg",
-    "space/space3.jpg",
-    "space/space4.jpg",
-    "space/space5.jpg",
-    "space/space6.jpg",
-  ]);
-
-  // const texture = loader.load([
-  // 	"space/space1.jpg",
-  // 	"space/space1.jpg",
-  // 	"space/space1.jpg",
-  // 	"space/space1.jpg",
-  // 	"space/space1.jpg",
-  // 	"space/space1.jpg",
-  //   ]);
-
-  // Set the scene background property to the resulting texture.
-  scene.background = texture;
-  // highlight-end
-  return null;
-}
-
-// Geometry
 function Sphere() {
-  // highlight-start
-  const { scene, gl } = useThree();
-  // The cubeRenderTarget is used to generate a texture for the reflective sphere.
-  // It must be updated on each frame in order to track camera movement and other changes.
-  const cubeRenderTarget = new WebGLCubeRenderTarget(256, {
-    format: RGBAFormat,
-    generateMipmaps: true,
-    minFilter: LinearMipmapLinearFilter,
-  });
-  const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget);
-  cubeCamera.position.set(0, 100, 0);
-  scene.add(cubeCamera);
-
-  // Update the cubeCamera with current renderer and scene.
-  useFrame(() => cubeCamera.update(gl, scene));
-  // highlight-end
-  return (
-    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]} castShadow>
-      <sphereGeometry attach="geometry" args={[2, 32, 32]} />
-      <meshBasicMaterial
-        attach="material"
-        envMap={cubeCamera.renderTarget.texture} // highlight-line
-        color="white"
-        //   roughness={0.1}
-        //   metalness={1}
-      />
-    </mesh>
-  );
-}
-
-function Box() {
-  const map = new THREE.TextureLoader().load("space/space.jpg");
-  const loader = new THREE.CubeTextureLoader();
-  // The CubeTextureLoader load method takes an array of urls representing all 6 sides of the cube.
-  const texture = loader.load([
-    "space/space1.jpg",
-    "space/space2.jpg",
-    "space/space3.jpg",
-    "space/space4.jpg",
-    "space/space5.jpg",
-    "space/space6.jpg",
+  const map = useTexture([
+    "space/space4.jpg", // pos-x
+    "space/space2.jpg", // neg-x
+    "space/space1.jpg", // pos-y
+    "space/space6.jpg", // neg-y
+    "space/space3.jpg", // pos-z
+    "space/space5.jpg", // neg-z
   ]);
   const meshRef: any = useRef();
-  const incRef = useRef(0.005);
   useFrame((state, delta) => {
     // adjust object position
     if (meshRef.current) {
-      meshRef.current.rotation.x += delta / 2;
-      meshRef.current.rotation.y += delta / 2;
+      // Perform your animation or modification logic here
+      for (
+        let i = 0, l = meshRef.current.geometry.attributes.position.count;
+        i < l;
+        i++
+      ) {
+        console.log("update...........");
+        const vertex = new THREE.Vector3();
+        vertex.fromBufferAttribute(
+          meshRef.current.geometry.attributes.position,
+          i
+        );
+        vertex.normalize().multiplyScalar(550);
+        meshRef.current.geometry.attributes.position.setXYZ(
+          i,
+          vertex.x,
+          vertex.y,
+          vertex.z
+        );
+      }
+
+      meshRef.current.geometry.attributes.position.needsUpdate = true;
     }
   });
 
   return (
     <mesh ref={meshRef}>
-     <sphereBufferGeometry args={[3, 64, 32]} />
-      <meshBasicMaterial map={map} side={THREE.BackSide} />
+      <boxBufferGeometry args={[300, 300, 300, 7, 7, 7]} />
+      {map.map((texture, idx) => (
+        <meshBasicMaterial
+          key={texture.id}
+          attach={`material-${idx}`}
+          map={texture}
+          side={THREE.BackSide}
+		  needsUpdate={true}
+		  
+        />
+      ))}
     </mesh>
   );
 }
 
-export default function App() {
+function App() {
   return (
     <Canvas
-      frameloop="demand"
-      style={{ height: "100vh" }}
-      camera={{ fov: 70, near: 0.01, far: 100, position: [0, 0, 1] }}
+      camera={{ fov: 70, near: 0.01, far: 1000, position: [0, 0, 1] }}
+      style={{ height: "100vh", backgroundColor: "#87CEEB" }}
     >
       <OrbitControls
         enableZoom={false}
@@ -150,13 +72,12 @@ export default function App() {
         autoRotate={true}
         rotateSpeed={-0.5}
       />
-      {/* <Suspense fallback={null}> */}
-      {/* <Preload all /> */}
-      {/* <CameraControls /> */}
-      {/* <Sphere /> */}
-      {/* <SkyBox/> */}
-      <Box />
-      {/* </Suspense> */}
+
+      <Suspense fallback={null}>
+        <Sphere />
+      </Suspense>
     </Canvas>
   );
 }
+
+export default App;
